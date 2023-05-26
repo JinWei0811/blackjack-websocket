8037bd0dca4616819173ee38950cf127425fa1e9
JinWei0811 <31277660+JinWei0811@users.noreply.github.com>
2023-05-26 16:57:08 +0800

    Update WebSocketExample.java

--- src/main/java/com/example/websocket/WebSocketExample.java
+++ src/main/java/com/example/websocket/WebSocketExample.java
@@ -6,10 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.LoggerFactoryFriend;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketMessage;
-import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.*;
 
 
 @Component
@@ -20,26 +17,25 @@ public class WebSocketExample implements WebSocketHandler {
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        session.sendMessage(new TextMessage("連接成功"));
         // 當建立連接時觸發，可以在這裡執行相關處理邏輯
-
     }
 
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
         logger.debug("Received " + message.getPayload());
+        session.sendMessage(new TextMessage(String.format("%s 您好", message.getPayload())));
         // 收到訊息時觸發，可以在這裡執行相關處理邏輯
     }
 
     @Override
     public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
         // 連接出錯時觸發，可以在這裡處理錯誤
-
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
         // 連接關閉時觸發，可以在這裡執行相關處理邏輯
-
     }
 
     @Override
